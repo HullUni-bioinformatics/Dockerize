b2e42339be870ef55fdf7031d29b882959b0f383
chrishah <chrisi.hahni@gmail.com>
2019-01-06 21:17:39 +0100

    fix bless kmc location problem

--- error-correction/BLESS/docker_helper.sh
+++ error-correction/BLESS/docker_helper.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
+. ~/.bashrc
 
-if [ -z "$@" ]; then
-	exec /src/bless/v1p02/bless
+#if [ -z "$@" ]; then
+#	exec /src/bless/v1p02/bless
+#else
+#	if [ "$1" == "/bin/bash" ]
+#	then
+#		echo -e "call bless via '/src/bless/v2p01/bless'"
+#		/bin/bash -c bash
+#	else
+#		exec /src/bless/v1p02/bless "$@"
+#	fi
+#fi
+
+if [ "$1" == "/bin/bash" ]
+then
+	/bin/bash -c bash
 else
-	if [ "$1" == "/bin/bash" ]
-	then
-		echo -e "call bless via '/src/bless/v2p01/bless'"
-		/bin/bash -c bash
-	else
-		exec /src/bless/v1p02/bless "$@"
-	fi
+	exec "$@"
 fi
